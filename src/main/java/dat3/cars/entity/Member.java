b29e16ea9ee57046ce43eede9674dd99bcde0e69
FrederikTC <fred47i4@stud.kea.dk>
2022-08-25 16:28:09 +0200

    udkommentering

--- src/main/java/dat3/cars/entity/Member.java
+++ src/main/java/dat3/cars/entity/Member.java
@@ -1,2 +1,29 @@
-package dat3.cars.entity;public class Member {
+package dat3.cars.entity;
+
+import dat3.security.entity.UserWithRoles;
+import org.springframework.security.core.userdetails.User;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class Member extends UserWithRoles{
+
+    String firstName;
+
+    public Member(String user, String password, String email, String firstName) {
+        super(user, password, email);
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Member() {
+    }
 }
--- src/main/java/dat3/cars/repository/MemberRepository.java
+++ src/main/java/dat3/cars/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package dat3.cars.repository;
 import dat3.cars.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MemeberRepository extends CrudRepository<Member,Integer> {
+public interface MemberRepository extends CrudRepository<Member,String> {
+    Member findMemberByEmail(String email);
 
 }
